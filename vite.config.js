1e9515c9d2818702e86d5e62bd7a5b3b095648f2
xcyyan <16528070+xcyyan@user.noreply.gitee.com>
2026-05-12 18:55:04 +0800

    add thoughts.html as multi-page build entry

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        thoughts: 'thoughts.html',
+      },
+    },
+  },
 })
