fd7bbb7d2e44e90bff8e4be3dc36475434066bf7
xcyyan520 <xcyyan520@github.com>
2026-06-08 17:56:17 +0800

    feat: rewrite App.vue and app.js for Star Desk layout

--- src/app.js
+++ src/app.js
@@ -1,342 +1,252 @@
-import homeright from '../src/components/hoemright.vue';
-import typewriter from './components/typewriter.vue';
-import tab1 from './components/tabs/tab1.vue';
-import tab2 from './components/tabs/tab2.vue';
-import tab3 from './components/tabs/tab3.vue';
-import loader from './components/loader.vue';
-import polarchart from './components/polarchart.vue';
-import config from './config.js';
-import { getCookie } from './utils/cookieUtils.js';
-import { setMeta,getFormattedTime,getFormattedDate,dataConsole } from './utils/common.js';
+import ProfileCard from './components/ProfileCard.vue'
+import DiaryCard from './components/DiaryCard.vue'
+import DiaryEditor from './components/DiaryEditor.vue'
+import CalendarDialog from './components/CalendarDialog.vue'
+import PhotoWall from './components/PhotoWall.vue'
+import TagsCard from './components/TagsCard.vue'
+import ProjectsCard from './components/ProjectsCard.vue'
+import ControlBar from './components/ControlBar.vue'
+import MiniMusicPlayer from './components/MiniMusicPlayer.vue'
+import StarfieldBackground from './components/StarfieldBackground.vue'
+
+import typewriter from './components/typewriter.vue'
+import loader from './components/loader.vue'
+import tab1 from './components/tabs/tab1.vue'
+import tab2 from './components/tabs/tab2.vue'
+import tab3 from './components/tabs/tab3.vue'
+
+import config from './config.js'
+import { getCookie } from './utils/cookieUtils.js'
+import { setMeta, getFormattedTime, getFormattedDate, dataConsole } from './utils/common.js'
 import { useDisplay } from 'vuetify'
 
 export default {
   components: {
-    tab1,tab2,tab3,loader,homeright,typewriter,polarchart
+    ProfileCard, DiaryCard, DiaryEditor, CalendarDialog, PhotoWall,
+    TagsCard, ProjectsCard, ControlBar, MiniMusicPlayer,
+    StarfieldBackground, typewriter, loader, tab1, tab2, tab3,
   },
   setup() {
-    const { xs,sm,md } = useDisplay();
-    return { xs,sm,md };
+    const { xs, sm, md } = useDisplay()
+    return { xs, sm, md }
   },
   data() {
     return {
-      isloading:false,
+      isloading: false,
       isClearScreen: false,
-      formattedTime:"",
-      formattedDate:"",
+      formattedTime: '',
+      formattedDate: '',
       configdata: config,
-      dialog1: false,
-      dialog2: false,
+      dialogSettings: false,
+      dialogAbout: false,
       personalizedtags: null,
       videosrc: '',
-      ismusicplayer: false,
-      isPlaying:false,
+      imageurl: '',
+      isPlaying: false,
       playlistIndex: 0,
       audioLoading: false,
       musicinfo: null,
-      musicinfoLoading:false,
-      lyrics:{},
+      musicinfoLoading: false,
+      lyrics: {},
       socialPlatformIcons: null,
-      isExpanded: false,
-      stackicons:[
-        {icon:"mdi-vuejs",color:"green", model: false,tip: 'vue'},
-        {icon:"mdi-language-javascript",color:"#CAD300", model: false,tip: 'javascript'},
-        {icon:"mdi-language-css3",color:"blue", model: false,tip: 'css'},
-        {icon:"mdi-language-html5",color:"red", model: false,tip: 'html'},
-        {icon:"$vuetify",color:"#1697F6", model: false,tip: 'vuetify'},
-      ],
-      projectcards:null,
+      projectcards: null,
       tab: null,
-      tabs: [
-        {
-          icon: 'mdi-pencil-plus',
-          text: '样式预览',
-          value: 'tab-1',
-          component: "tab1",
-        },
-        {
-          icon: 'mdi-wallpaper',
-          text: '背景预览',
-          value: 'tab-2',
-          component: "tab2",
-        },
-        {
-          icon: 'mdi-music-circle-outline',
-          text: '音乐播放',
-          value: 'tab-3',
-          component: "tab3",
-        },
+      settingTabs: [
+        { icon: 'mdi-pencil-plus', text: '样式预览', value: 'tab-1', component: 'tab1' },
+        { icon: 'mdi-wallpaper', text: '背景预览', value: 'tab-2', component: 'tab2' },
+        { icon: 'mdi-music-circle-outline', text: '音乐播放', value: 'tab-3', component: 'tab3' },
       ],
-
-    };
-  },
-  async mounted() {
-    if(import.meta.env.VITE_CONFIG){
-      this.configdata = JSON.parse(import.meta.env.VITE_CONFIG);
+      // Diary
+      showDiaryEditor: false,
+      showCalendar: false,
+      showPhotoWall: false,
+      todayDiary: null,
+      diaryLoading: false,
     }
-    this.projectcards = this.configdata.projectcards;this.socialPlatformIcons = this.configdata.socialPlatformIcons;
-    this.personalizedtags = this.configdata.tags;
-    this.isloading = true;
-    let imageurl = "";
-    this.dataConsole();
-    this.setMeta(this.configdata.metaData.title,this.configdata.metaData.description,this.configdata.metaData.keywords,this.configdata.metaData.icon);
-    
-    imageurl = this.setMainProperty(imageurl);
-
-    //异步等待背景壁纸包括视频壁纸加载完成后再显示页面
-    const loadImage = () => {
-        const imageUrls = [
-          config.avatar,
-          ...config.projectcards.map(item => item.img)
-        ];
-        return new Promise((resolve, reject) => {
-          const imagePromises = imageUrls.map((url) => {
-            return new Promise((resolve, reject) => {
-                const imgs = new Image();
-                imgs.src = url;
-                imgs.onload = () => resolve();
-                imgs.onerror = (err) => reject(err);
-            });
-          })
-
-          // 设置超时机制：2.5秒
-          const timeoutPromise = new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-            }, 2500);
-          });
-          
-          // 等待所有图片加载完成或超时
-          Promise.race([Promise.all(imagePromises), timeoutPromise]).then(()=>{
-            if(imageurl){
-              const img = new Image();
-              img.src = imageurl;
-              // resolve() 函数通将一个 Promise 对象从未完成状态转变为已完成状态
-              img.onload = () => {resolve();};
-              img.onerror = (err) => {reject(err);};
-            }else{
-              const video = this.$refs.VdPlayer;
-              video.onloadedmetadata = () => {
-                setTimeout(() => {
-                }, "200");  
-                resolve();
-              };
-              video.onerror = (err) => {resolve();};
-            }
-          })
-        });
-     };
-
-    loadImage().then(() => {
-        this.formattedTime =  this.getFormattedTime(new Date());
-        this.formattedDate =  this.getFormattedDate(new Date());
-        setTimeout(() => {
-          this.isloading = false;
-        }, "500");          
-      }).catch((err) => {
-        console.error('壁纸加载失败:', err);
-        setTimeout(() => {
-          this.isloading = false;
-        }, "100");  
-      });
- 
-      setInterval(() => {
-        this.formattedTime =  this.getFormattedTime(new Date()) ;
-      }, 1000);
-
-      await this.getMusicInfo();  //获取音乐数据
-      this.setupAudioListener();  //设置 ended 事件监听器，当歌曲播放结束时自动调用 nextTrack 方法。
   },
-
-  beforeDestroy() {     //在组件销毁前移除事件监听器，防止内存泄漏。
-    this.$refs.audioPlayer.removeEventListener('ended',  this.nextTrack);
-  },
-
-  watch:{
-    isClearScreen(val){
-      if(!this.videosrc){
-        return
-      }
-      if(val){
-        this.$refs.VdPlayer.style.zIndex = 0; 
-        this.$refs.VdPlayer.controls = true;
-      }else{
-        this.$refs.VdPlayer.style.zIndex = -100; 
-        this.$refs.VdPlayer.controls = false;
-      }
-    },
-    audioLoading(val){
-      this.isPlaying = !val;
-    }
-
-  //若弹出框使得页面播放卡顿，可以先停止背景播放
-  //   dialog1(val){
-  //     if(val){
-  //       this.$refs.VdPlayer.pause();
-  //     }else{
-  //       this.$refs.VdPlayer.play();
-  //     }
-  //  }
-  },
-
   computed: {
     currentSong() {
-      return this.musicinfo[this.playlistIndex];
+      return this.musicinfo?.[this.playlistIndex] || null
     },
     audioPlayer() {
-      return this.$refs.audioPlayer;
-    }
+      return this.$refs.audioPlayer
+    },
+    appFrameStyle() {
+      return this.xs
+        ? { height: '100%', width: '100%', top: '0', left: '0' }
+        : this.sm
+        ? { height: '98%', width: '98%', top: '1%', left: '1%' }
+        : { height: '96.6%', width: '99%', top: '1.7%', left: '0.5%' }
+    },
+    deskLeftStyle() {
+      return this.xs ? {} : { width: '36%' }
+    },
+    deskRightStyle() {
+      return this.xs ? {} : { width: '52%', marginTop: '0' }
+    },
   },
-  
+  async mounted() {
+    if (import.meta.env.VITE_CONFIG) {
+      this.configdata = JSON.parse(import.meta.env.VITE_CONFIG)
+    }
+    this.projectcards = this.configdata.projectcards
+    this.socialPlatformIcons = this.configdata.socialPlatformIcons
+    this.personalizedtags = this.configdata.tags
+    this.isloading = true
+
+    this.dataConsole()
+    this.setMeta(this.configdata.metaData.title, this.configdata.metaData.description, this.configdata.metaData.keywords, this.configdata.metaData.icon)
+
+    this.setMainProperty()
+
+    // Load wallpaper + wait for it
+    await this.initBackground()
+    this.formattedTime = this.getFormattedTime(new Date())
+    this.formattedDate = this.getFormattedDate(new Date())
+    setTimeout(() => { this.isloading = false }, 500)
+
+    setInterval(() => {
+      this.formattedTime = this.getFormattedTime(new Date())
+    }, 1000)
+
+    await this.getMusicInfo()
+    this.setupAudioListener()
+    await this.loadTodayDiary()
+  },
+  beforeDestroy() {
+    this.$refs.audioPlayer?.removeEventListener('ended', this.nextTrack)
+  },
+  watch: {
+    isClearScreen(val) {
+      if (!this.videosrc) return
+      if (val) {
+        this.$refs.VdPlayer.style.zIndex = 0
+        this.$refs.VdPlayer.controls = true
+      } else {
+        this.$refs.VdPlayer.style.zIndex = -100
+        this.$refs.VdPlayer.controls = false
+      }
+    },
+    audioLoading(val) {
+      this.isPlaying = !val
+    },
+  },
   methods: {
-    getCookie,setMeta,getFormattedTime,getFormattedDate,dataConsole,
+    getCookie, setMeta, getFormattedTime, getFormattedDate, dataConsole,
 
-    setMainProperty(imageurl){
-      const root = document.documentElement;
-      let leleodata = this.getCookie("leleodata");
-      if(leleodata){
-        root.style.setProperty('--leleo-welcomtitle-color', `${leleodata.color.welcometitlecolor}`);
-        root.style.setProperty('--leleo-vcard-color', `${leleodata.color.themecolor}`);
-        root.style.setProperty('--leleo-brightness', `${leleodata.brightness}%`);
-        root.style.setProperty('--leleo-blur', `${leleodata.blur}px`); 
-      }else{
-        root.style.setProperty('--leleo-welcomtitle-color', `${this.configdata.color.welcometitlecolor}`);
-        root.style.setProperty('--leleo-vcard-color', `${this.configdata.color.themecolor}`);  
-        root.style.setProperty('--leleo-brightness', `${this.configdata.brightness}%`);  
-        root.style.setProperty('--leleo-blur', `${this.configdata.blur}px`);
-      }
-  
-      let leleodatabackground = this.getCookie("leleodatabackground");
-      const { xs } = useDisplay();
-      if(leleodatabackground){
-        if(xs.value){
-          if(leleodatabackground.mobile.type == "pic"){
-            root.style.setProperty('--leleo-background-image-url', `url('${leleodatabackground.mobile.datainfo.url}')`);
-            imageurl = leleodatabackground.mobile.datainfo.url;
-            return imageurl;
-          }else{
-            this.videosrc = leleodatabackground.mobile.datainfo.url;
-          }
-        }else{
-          if(leleodatabackground.pc.type == "pic"){
-            root.style.setProperty('--leleo-background-image-url', `url('${leleodatabackground.pc.datainfo.url}')`);
-            imageurl = leleodatabackground.pc.datainfo.url;
-            return imageurl;
-          }else{
-            this.videosrc = leleodatabackground.pc.datainfo.url;
-          }
-        }
-          
-      }else{
-        if(xs.value){
-          if(this.configdata.background.mobile.type == "pic"){
-            root.style.setProperty('--leleo-background-image-url', `url('${this.configdata.background.mobile.datainfo.url}')`);
-            imageurl = this.configdata.background.mobile.datainfo.url;
-            return imageurl;
-          }else{
-            this.videosrc = this.configdata.background.mobile.datainfo.url;
-          }
-        }else{
-          if(this.configdata.background.pc.type == "pic"){
-            root.style.setProperty('--leleo-background-image-url', `url('${this.configdata.background.pc.datainfo.url}')`);
-            imageurl = this.configdata.background.pc.datainfo.url;
-            return imageurl;
-          }else{
-            this.videosrc = this.configdata.background.pc.datainfo.url;
-          }
-          
-        }
+    setMainProperty() {
+      const root = document.documentElement
+      let leleodata = this.getCookie('leleodata')
+      if (leleodata) {
+        root.style.setProperty('--leleo-welcomtitle-color', leleodata.color.welcometitlecolor)
+        root.style.setProperty('--leleo-vcard-color', leleodata.color.themecolor)
+        root.style.setProperty('--leleo-brightness', leleodata.brightness + '%')
+        root.style.setProperty('--leleo-blur', leleodata.blur + 'px')
+      } else {
+        root.style.setProperty('--leleo-welcomtitle-color', this.configdata.color.welcometitlecolor)
+        root.style.setProperty('--leleo-vcard-color', this.configdata.color.themecolor)
+        root.style.setProperty('--leleo-brightness', this.configdata.brightness + '%')
+        root.style.setProperty('--leleo-blur', this.configdata.blur + 'px')
       }
     },
 
-    projectcardsShow(key){
-      this.projectcards.forEach((item,index)=>{
-        if(index!= key){
-          item.show = false;
-        }
-      })
+    async initBackground() {
+      const root = document.documentElement
+      let leleodatabackground = this.getCookie('leleodatabackground')
+      const bg = leleodatabackground || this.configdata.background
+      const device = this.xs ? 'mobile' : 'pc'
+
+      if (bg?.[device]?.type === 'pic') {
+        root.style.setProperty('--leleo-background-image-url', `url('${bg[device].datainfo.url}')`)
+        this.imageurl = bg[device].datainfo.url
+      } else if (bg?.[device]?.type === 'video') {
+        this.videosrc = bg[device].datainfo.url
+      }
+
+      // Wait for background to load
+      if (this.imageurl) {
+        await new Promise((resolve) => {
+          const img = new Image()
+          img.onload = resolve
+          img.onerror = resolve
+          img.src = this.imageurl
+          setTimeout(resolve, 2500)
+        })
+      } else if (this.videosrc) {
+        await new Promise((resolve) => {
+          const video = this.$refs.VdPlayer
+          if (video) {
+            video.onloadedmetadata = () => setTimeout(resolve, 200)
+            video.onerror = resolve
+          }
+          setTimeout(resolve, 2500)
+        })
+      }
     },
-    handleCancel(){
-      this.dialog1 = false;
-    },
-    jump(url){
-      window.open(url, '_blank').focus();
-    },
-    
-    async getMusicInfo(){
-      this.musicinfoLoading = true;
+
+    // Music (kept from original)
+    async getMusicInfo() {
+      this.musicinfoLoading = true
       try {
-        const response = await fetch(`https://api.i-meto.com/meting/api?server=${this.configdata.musicPlayer.server}&type=${this.configdata.musicPlayer.type}&id=${this.configdata.musicPlayer.id}`
-        );
-        if (!response.ok) {
-          throw new Error('网络请求失败');
-        }
-        this.musicinfo = await response.json();
-        this.musicinfoLoading = false;
+        const response = await fetch(`https://api.i-meto.com/meting/api?server=${this.configdata.musicPlayer.server}&type=${this.configdata.musicPlayer.type}&id=${this.configdata.musicPlayer.id}`)
+        if (!response.ok) throw new Error('Network error')
+        this.musicinfo = await response.json()
+        this.musicinfoLoading = false
       } catch (error) {
-        console.error('请求失败:', error);
+        console.error('Music load failed:', error)
       }
-      
     },
-    musicplayershow(val) {
-        this.ismusicplayer = val;
-    },
-
     setupAudioListener() {
-      this.$refs.audioPlayer.addEventListener('ended', this.nextTrack);
+      this.$refs.audioPlayer?.addEventListener('ended', this.nextTrack)
     },
-
     togglePlay() {
       if (!this.isPlaying) {
-        this.audioPlayer.play();
-        this.isVdMuted = true;
+        this.audioPlayer?.play()
       } else {
-        this.audioPlayer.pause();
-        this.isVdMuted = false;
+        this.audioPlayer?.pause()
       }
-      this.isPlaying = !this.musicinfoLoading && !this.isPlaying;
+      this.isPlaying = !this.musicinfoLoading && !this.isPlaying
     },
     previousTrack() {
-      this.playlistIndex = this.playlistIndex > 0 ? this.playlistIndex - 1 : this.musicinfo.length - 1;
-      this.updateAudio();
+      this.playlistIndex = this.playlistIndex > 0 ? this.playlistIndex - 1 : (this.musicinfo?.length || 1) - 1
+      this.updateAudio()
     },
     nextTrack() {
-      this.playlistIndex = this.playlistIndex < this.musicinfo.length - 1 ? this.playlistIndex + 1 : 0;
-      this.updateAudio();
+      this.playlistIndex = this.playlistIndex < (this.musicinfo?.length || 1) - 1 ? this.playlistIndex + 1 : 0
+      this.updateAudio()
     },
     updateAudio() {
-      this.audioPlayer.src = this.currentSong.url;
-      this.$refs.audiotitle.innerText = this.currentSong.title;
-      this.$refs.audioauthor.innerText = this.currentSong.author;
-      this.isPlaying = true;
-      this.audioPlayer.play();
+      if (this.audioPlayer && this.currentSong) {
+        this.audioPlayer.src = this.currentSong.url
+        this.isPlaying = true
+        this.audioPlayer.play()
+      }
     },
-    updateCurrentIndex(index) {
-      this.playlistIndex = index;
-      this.updateAudio();
+    updateCurrentIndex(index) { this.playlistIndex = index; this.updateAudio() },
+    updateIsPlaying(isPlaying) { this.isPlaying = isPlaying },
+    updateLyrics(lyrics) { this.lyrics = lyrics },
+    onWaiting() { this.audioLoading = true },
+    onCanPlay() { this.audioLoading = false },
+
+    // UI actions
+    openSettings() { this.dialogSettings = true },
+    openThoughts() { window.location.href = '/thoughts.html' },
+    openWallpaper() { this.tab = 'tab-2'; this.dialogSettings = true },
+    openMusicPlayer() { this.tab = 'tab-3'; this.dialogSettings = true },
+    toggleClearScreen() { this.isClearScreen = !this.isClearScreen },
+
+    // Diary
+    async loadTodayDiary() {
+      this.diaryLoading = true
+      const today = new Date().toISOString().slice(0, 10)
+      try {
+        const res = await fetch(`/api/diary?date=${today}`)
+        if (res.ok) {
+          const data = await res.json()
+          this.todayDiary = data
+        }
+      } catch (e) { /* ignore */ }
+      this.diaryLoading = false
     },
-    updateIsPlaying(isPlaying) {
-      this.isPlaying = isPlaying;
-    },
-    updateLyrics(lyrics){
-      this.lyrics = lyrics;
-    },
-    // 监听等待事件（缓冲不足）
-    onWaiting() {
-      this.audioLoading = true;
-    },
-    // 监听可以播放事件（缓冲足够）
-    onCanPlay() {
-      this.audioLoading = false;
-    },
-    expandSwitch() {
-      this.isExpanded = true;
-    },
-    collapseSwitch() {
-      this.isExpanded = false;
-    },
-    openThoughts() {
-      window.location.href = '/thoughts.html';
-    },
-  }
-};
+  },
+}
